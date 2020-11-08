bcf8d0643278de9930e28ad9e7dac7bb427421c0
google88888888 <571343428@qq.com>
2020-11-08 14:57:09 +0800

    1

---
+++ jj易教网页黄若芸/20201108/homework1/js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	
+	$("#bottomId") .click(function(){
+		$("#contentId").hide();
+	});
+
+});
---
+++ jj易教网页黄若芸/20201108/homework2/js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	
+	$("#yellow") .click(function(){
+		$(".item-yellow").css("background-color","red");
+	});
+	
+	$("#green") .click(function(){
+		$(".item-green").css("background-color","red");
+	});
+	
+	$("#purple") .click(function(){
+		$(".item-purple").css("background-color","red");
+	});
+});
