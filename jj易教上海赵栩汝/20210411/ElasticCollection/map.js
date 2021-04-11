8cdddfde5847f0824472a470667a468c71ddf24a
google88888888 <571343428@qq.com>
2021-04-11 23:51:39 +0800

    feat: 布局

--- jj易教上海赵栩汝/20210411/ElasticCollection/map.js
+++ jj易教上海赵栩汝/20210411/ElasticCollection/map.js
@@ -1,15 +1,46 @@
+var Airtable = require('airtable');
 
-var closeId = document.querySelector('#closeId');
-var overTipsId = document.querySelector('#overTipsId');
-var nextId = document.querySelector('#nextId');
+Airtable.configure({
+  apiKey: 'keyFxvwCW8U8MxKns',
+  endpointUrl: 'https://api.airtable.com'
+});
+var base = Airtable.base('apppLaTVMWcNaWWa8');
+
+base('Table 1').select({
+  maxRecords: 200,
+  view: 'Grid view'
+}).eachPage(function page(records, fetchNextPage) {
+  var leftId = document.querySelector('#leftId');
+
+  console.log('11111111111111111111111');
+  console.log(records);
+  var stateArray=[];
+  for(var i=0;i<records.length;i++){
+    var hasValueInStateArray=false
+    for(var j=0;j<stateArray.length;j++){
+      if(records[i].fields.state==stateArray[j]){
+        hasValueInStateArray=true;
+        break;
+      }
+    }
+    if(hasValueInStateArray===false){
+      stateArray.push(records[i].fields.state);
+    }
+  }
+
+  console.log('22222222222222222222222');
+  console.log(stateArray);
+  for(var x=0;x<stateArray.length;x++){
+      console.log('33333333');
+    console.log(stateArray[x]);
+    var leftItem = document.createElement('div');
+    leftItem.classList.add("leftItem");
+    leftItem.innerHTML = stateArray[x];
+    leftId.append(leftItem);
+  }
 
 
-
-closeId.addEventListener('click',function(){
-    overTipsId.classList.add('hide');
-})
-
-
-nextId.addEventListener('click',function(){
-    location.href="./"
-})
+  fetchNextPage();
+}, function done(err) {
+  if (err) { console.error(err); return; }
+});
