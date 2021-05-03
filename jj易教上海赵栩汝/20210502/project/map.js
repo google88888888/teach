55e88e87e58ed6eccf0e987807413bd6a9a9ce4d
mengjiuxiang <571343428@qq.com>
2021-05-03 11:58:17 +0800

    随即水果

--- jj易教上海赵栩汝/20210502/project/map.js
+++ jj易教上海赵栩汝/20210502/project/map.js
@@ -1,6 +1,9 @@
 
 console.log('find me in main.js');
-
+//生成从minNum到maxNum的随机数
+function randomNum(minNum,maxNum){ 
+  return parseInt(Math.random()*(maxNum-minNum+1)+minNum,10); 
+} 
 var container = document.querySelector('.container');
 
 var place = document.createElement('div');
@@ -19,10 +22,31 @@ var initImages=[
   './fruit/R.png',
   './fruit/N.png'
 ]
+var allFruitImages=[
+  './fruit/apple.png',
+  './fruit/banana.png',
+  './fruit/cherrytomato.png',
+  './fruit/grape.png',
+  './fruit/lemon.png',
+  './fruit/lychee.png',
+  './fruit/mango.png',
+  './fruit/orange.png',
+  './fruit/peach.png',
+  './fruit/pear.png',
+  './fruit/pineapple.png',
+  './fruit/strawberry.png',
+  './fruit/watermelon.png',
+]
 for(var i=0;i<initImages.length;i++){
   var photoUrl = document.createElement('img');
   photoUrl.classList.add('item');
   photoUrl.src = initImages[i];
+  
+  photoUrl.addEventListener('mouseover',function(){
+    let index=randomNum(0,12);
+    this.src=allFruitImages[index];
+  })
+
   container.append(photoUrl);
 }
 
@@ -30,5 +54,11 @@ for(var i=0;i<78;i++){
   var photoUrl = document.createElement('img');
   photoUrl.classList.add('item');
   photoUrl.src = './fruit/plate.png';
+
+  photoUrl.addEventListener('mouseover',function(){
+    let index=randomNum(0,12);
+    this.src=allFruitImages[index];
+  })
+
   container.append(photoUrl);
 }
