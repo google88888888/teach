4d247394ff061661ae3b8bb0674bb9147e6a59eb
mengjiuxiang <571343428@qq.com>
2021-05-04 14:00:33 +0800

    websocket

---
+++ jj易教上海赵栩汝/websocket/excited-winter-taker-2021-05-04_055322/excited-winter-taker-2021-05-04_055322/app/app.js
@@ -0,0 +1,47 @@
+var express = require('express')
+  , app = express()
+  , http = require('http')
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server)
+  , path = require('path')
+  
+// Start server
+server.listen(3000);
+
+// Set up 'public' folder 
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Point / to index.html (could just put index.html in public but leaving for reference)
+app.use('/', function(req, res, next){
+  res.sendFile('public/html/index.html', { root : __dirname })
+})
+
+// Users array
+var users = [];
+
+// Create new websocket 
+io.sockets.on('connection', function (socket) {
+  // Set user name
+  var userName = 'user ' + (users.length + 1);
+  socket.userName =  userName
+  
+  users.push(userName); //add to array
+  io.sockets.emit('updateUsers', users); // update list			
+
+  //user msgs
+  socket.on('emit-msg', function (msg) {
+    console.log(socket.userName + ':', msg);
+    io.sockets.emit('broadcast-msg', msg);
+  });
+
+  //user disconnection
+  socket.on('disconnect', function() {
+    //remove from array
+    var pos = users.indexOf(socket.userName);
+    if (pos >= 0) {
+      users.splice(pos, 1);
+    };
+    io.sockets.emit('updateUsers', users);	// update list
+    console.log('User disconnected')
+  });
+});
