ade67059e52d628de4e00c2c66584eb8e1dc950e
mengjiuxiang <571343428@qq.com>
2021-05-05 12:19:28 +0800

    1

---
+++ jj易教上海赵栩汝/20210502/InputOutput/map.js
@@ -0,0 +1,64 @@
+
+console.log('find me in main.js');
+//生成从minNum到maxNum的随机数
+function randomNum(minNum,maxNum){ 
+  return parseInt(Math.random()*(maxNum-minNum+1)+minNum,10); 
+} 
+var container = document.querySelector('.container');
+
+var place = document.createElement('div');
+place.classList.add("container");
+document.querySelector(".container").append(place);
+
+var initImages=[
+  './fruit/L.png',
+  './fruit/E.png',
+  './fruit/T.png',
+  './fruit/S.png',
+  './fruit/plate.png',
+  './fruit/L.png',
+  './fruit/E.png',
+  './fruit/A.png',
+  './fruit/R.png',
+  './fruit/N.png'
+]
+var allFruitImages=[
+  './fruit/apple.png',
+  './fruit/banana.png',
+  './fruit/cherrytomato.png',
+  './fruit/grape.png',
+  './fruit/lemon.png',
+  './fruit/lychee.png',
+  './fruit/mango.png',
+  './fruit/orange.png',
+  './fruit/peach.png',
+  './fruit/pear.png',
+  './fruit/pineapple.png',
+  './fruit/strawberry.png',
+  './fruit/watermelon.png',
+]
+for(var i=0;i<initImages.length;i++){
+  var photoUrl = document.createElement('img');
+  photoUrl.classList.add('item');
+  photoUrl.src = initImages[i];
+  
+  photoUrl.addEventListener('mouseover',function(){
+    let index=randomNum(0,12);
+    this.src=allFruitImages[index];
+  })
+
+  container.append(photoUrl);
+}
+
+for(var i=0;i<78;i++){
+  var photoUrl = document.createElement('img');
+  photoUrl.classList.add('item');
+  photoUrl.src = './fruit/plate.png';
+
+  photoUrl.addEventListener('mouseover',function(){
+    let index=randomNum(0,12);
+    this.src=allFruitImages[index];
+  })
+
+  container.append(photoUrl);
+}
