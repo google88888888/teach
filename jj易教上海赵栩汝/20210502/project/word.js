cc4dde9d8a3a55470f298b3abd15af314a9fad91
mengjiuxiang <571343428@qq.com>
2021-05-05 15:59:01 +0800

    done

--- jj易教上海赵栩汝/20210502/project/word.js
+++ jj易教上海赵栩汝/20210502/project/word.js
@@ -19,7 +19,10 @@ var secondId = document.querySelector('#secondId');
 var thirdId = document.querySelector('#thirdId');
 var fruitId = document.querySelector('#fruitId');
 var nameId = document.querySelector('#nameId');
-
+var soundId = document.querySelector('#soundId');
+var submitId = document.querySelector('#submitId');
+var inputId = document.querySelector('#inputId');
+var reviewId = document.querySelector('#reviewId');
 
 
 
@@ -46,7 +49,14 @@ function getSelectImg(selectImgIndex){
 }
 
 var selectImgIndex=0;
+let currentFruit='apple';
 getSelectImg(selectImgIndex);
+fruitId.innerHTML='';
+var fruitImg = document.createElement('img');
+fruitImg.classList.add('fruitImg');
+fruitImg.src='./fruit/'+currentFruit+'.png';
+fruitId.append(fruitImg);
+nameId.innerHTML=currentFruit;
 
 
 
@@ -65,6 +75,7 @@ downId.addEventListener('click',function(){
   getSelectImg(selectImgIndex);
 })
 
+
 firstId.addEventListener('click',function(){
   fruitId.innerHTML='';
   var fruitImg = document.createElement('img');
@@ -72,6 +83,7 @@ firstId.addEventListener('click',function(){
   fruitImg.src='./fruit/'+allFruits[selectImgIndex]+'.png';
   fruitId.append(fruitImg);
   nameId.innerHTML=allFruits[selectImgIndex];
+  currentFruit=allFruits[selectImgIndex];
 })
 secondId.addEventListener('click',function(){
   fruitId.innerHTML='';
@@ -80,6 +92,7 @@ secondId.addEventListener('click',function(){
   fruitImg.src='./fruit/'+allFruits[selectImgIndex+1]+'.png';
   fruitId.append(fruitImg);
   nameId.innerHTML=allFruits[selectImgIndex+1];
+  currentFruit=allFruits[selectImgIndex+1];
 })
 thirdId.addEventListener('click',function(){
   fruitId.innerHTML='';
@@ -88,4 +101,24 @@ thirdId.addEventListener('click',function(){
   fruitImg.src='./fruit/'+allFruits[selectImgIndex+2]+'.png';
   fruitId.append(fruitImg);
   nameId.innerHTML=allFruits[selectImgIndex+2];
+  currentFruit=allFruits[selectImgIndex+2];
+})
+
+soundId.addEventListener('click',function(){
+  var mp3 = "./sound/"+currentFruit+".mp3";
+  var mp3 = new Audio(mp3);
+  mp3.play();
+})
+
+submitId.addEventListener('click',function(){
+  if(inputId.value!==undefined&&inputId.value!==null&&inputId.value.toString().trim()==currentFruit){
+    var mp3 = "./sound/"+currentFruit+".mp3";
+    var mp3 = new Audio(mp3);
+    mp3.play();
+  }
+
+})
+
+reviewId.addEventListener('click',function(){
+  location.href="./map.html"
 })
