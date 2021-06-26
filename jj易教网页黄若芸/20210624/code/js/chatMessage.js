1a910cc3413b09e4d5abf686aa332057a2c00f76
google88888888 <571343428@qq.com>
2021-06-26 16:39:09 +0800

    feat: chat

--- jj易教网页黄若芸/20210624/code/js/chatMessage.js
+++ jj易教网页黄若芸/20210624/code/js/chatMessage.js
@@ -1,22 +1,20 @@
 $(document).ready(function(){
-    var navigator1 = document.getElementById("navigator1");
-    navigator1.addEventListener("click", function(){
-        window.location.href="./home.html"; 
-    });
-    var navigator2 = document.getElementById("navigator2");
-    navigator2.addEventListener("click", function(){
-        window.location.href="./shelf.html"; 
-    });
-    var navigator3 = document.getElementById("navigator3");
-    navigator3.addEventListener("click", function(){
-        window.location.href="./chat.html"; 
-    });
-    var navigator4 = document.getElementById("navigator4");
-    navigator4.addEventListener("click", function(){
-        window.location.href="./my.html"; 
+    $("#send").click(function(){
+        var messageInput=$("#messageInput").val();
+        $("#center").append(
+            '<div class="rightItem">'+
+            '<div class="rightImg">'+
+                '<img src="./image/right.png" class="rightImgPng" />'+
+            '</div>'+
+            '<div class="rightContent">'+
+                messageInput+
+            '</div>'+
+            '<div class="clear"></div>'+
+            '</div>'
+        )
     });
 
-    $(".item").click(function(){
-        window.location.href="./chatMessage.html"; 
+    $("#return,#close").click(function(){
+        window.location.href="./chat.html"; 
     });
 });
