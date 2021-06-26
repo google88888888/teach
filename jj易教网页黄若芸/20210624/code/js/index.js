7cbedad7539944347b232d6f281e7d67c2d0fb1c
google88888888 <571343428@qq.com>
2021-06-26 14:15:22 +0800

    click

--- jj易教网页黄若芸/20210624/code/js/index.js
+++ jj易教网页黄若芸/20210624/code/js/index.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
+    var signIn = document.getElementById("signIn");
+    signIn.addEventListener("click", function(){
+        window.location.href="./signIn.html"; 
+    });
 
+    var signUp = document.getElementById("signUp");
+    signUp.addEventListener("click", function(){
+        window.location.href="./signUp.html"; 
+    });
 });
